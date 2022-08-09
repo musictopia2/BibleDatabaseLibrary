28ccb3f848617e4b104caf0a4ccbb642d026c9e5
Andy Walter <musictopia2@hotmail.com>
2022-08-09 13:43:32 -0700

    Added support to serialize book context. Also, added new model to allow more flexible reading schedules.  This should make it where it works well whether the reading is new testament only, old testament only, or any other formats.  The key is storing information to allow reading several chapters in a group.

---
+++ Entities/ChapterRange.cs
@@ -0,0 +1,11 @@
+﻿namespace BibleDatabaseLibrary.Entities;
+/// <summary>
+/// the purpose of this is to be used for a bible reading schedule.  does not matter what is chosen for this single group item.
+/// </summary>
+public class ChapterRange
+{
+    public int ChapterFrom { get; set; }
+    public int ChapterTo { get; set; }
+    public string BookName { get; set; } = "";
+    public bool Completed { get; set; } //sometimes i need this and sometimes i don't.  does not hurt to put.  if not needed, then gets ignored.
+}
